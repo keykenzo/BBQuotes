708cfc69bdcf63a9edc41e8b87ad4e4aa6a0f7ea
Mario Duarte <mariods7@gmail.com>
2026-02-02 09:00:11 -0800

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Mario Duarte on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
